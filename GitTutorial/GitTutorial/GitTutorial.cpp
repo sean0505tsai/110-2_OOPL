70eb713f09130c3c6e220ce2c94e8ef9431e9ad5
kwochengho <kwochengho1020318@gmail.com>
2022-03-17 21:01:10 +0800

    step8-A-1

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -4,15 +4,7 @@
 #include <ctime>
 
 bool Guess(int number) {
-	static int target =1;
-	srand(time(NULL));
-	if(target== -1){
-		target =rand()%100+1;
-	}
-	if(number>target){
-		std::out<<"Bigger"<<std::endl;
-		return false;
-	}
+	
 	return false;
 }
 
