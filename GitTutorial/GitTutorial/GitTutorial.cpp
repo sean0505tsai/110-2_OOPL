0491e8bfc4820dac587be84033128d927179a49e
sean0505tsai <t108820018@ntut.org.tw>
2022-03-17 23:21:24 +0800

    step 8B-2

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -4,6 +4,7 @@
 #include <ctime>
 
 bool Guess(int number) {
+
 	static int target = -1;
 
 	srand(time(NULL));
@@ -12,8 +13,13 @@ bool Guess(int number) {
 	}
 
 	if (number == target)
+	{
+		std::cout <<
+			"Correct !!"; target = -1;
 		return true;
-
+	}
+	else if (number > target) std::cout << "Smaller" << std::endl;
+	else if (number < target) std::cout << "Bigger" << std::endl;
 	return false;
 }
 
@@ -21,6 +27,7 @@ int main()
 {
 	int guess;
 	do{
+		std::cout<<"Choose a number between 1 - 100";
 		std::cin>>guess;
 	}while(!Guess(guess));
 	return 0;
